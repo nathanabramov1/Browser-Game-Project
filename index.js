6fd2b5d475a8550e1ea935ce0ef4f110af50ba43
Nathaniel Abramov <abramovnatanel@gmail.com>
2023-06-16 17:10:08 -0400

    not finished, have two problems with my code, added javascript and css

--- index.js
+++ index.js
@@ -33,12 +33,9 @@ function addEventListeners(){
     cells.forEach((cell,index) => {
         cell.addEventListener('click', () => {
             if (board[index] === null) {
-                board[index] = currentPlayer
-                cell.innerText = currentPlayer
-                cell.classList.add('active')
+                updateBoard(index)
                 checkForWinner()
                 checkForTie()
-                switchplayer()
             }
         })
        
@@ -68,7 +65,7 @@ function checkForWinner() {
     [1,4,7],
     [2,5,8],
     [0,4,8],
-    [2,4,6]
+    [2,4,6],
   ]
 
   let winner = null
@@ -86,6 +83,16 @@ function checkForWinner() {
     removeEventListeners()
   }
 }
+
+// hightlight the winning cells
+
+function highlightWinnerCells(combination) {
+    let [a,b,c] = combination
+    cells[a].classList.add('winner')
+    cells[b].classList.add('winner')
+    cells[c].classList.add('winner')
+    removeEventListeners()
+}
 //check for a tie
 
 function checkForTie() {
@@ -94,11 +101,42 @@ function checkForTie() {
     removeEventListeners()
 }
 }
+
 //update the board after each move
 function updateBoard(index) {
     board[index] = currentPlayer
     let cell = cells[index]
     cell.innerText = currentPlayer
     cell.classList.add('active')
+    switchplayer()
 }
+
+// remove event listeners from the cells
+
+function removeEventListeners() {
+    cells.forEach(cell => {
+        cell.removeEventListener('click',switchplayer)
+    })
+}
+
+//restart the game
+
+function restartGame() {
+    board = [
+        null,null, null,
+        null,null, null,
+        null,null, null
+    ]
+    cells.forEach((cell) => {
+        cell.innerText = ''
+        cell.classList.remove('active', 'winner')
+    })
+    currentPlayer = determineFirstPlayer()
+    displaymessage(`player ${currentPlayer}'s turn!`)
+    addEventListeners()
+}
+
+                               //Problems
+//remove event listener does not work
+//if u win on the last cell it says its a tie, it should display a winner message
 
